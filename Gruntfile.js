cddaf80e122f409fd5572b8d9bd170e181aa0c11
rjp2525 <me@renophilibert.com>
2014-09-18 19:29:49 -0400

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		less: {
+			development: {
+				options: {
+					paths: ["./dist/css"],
+					yuicompress: true
+				},
+			files: {
+				"./dist/css/style.css": "./less/style.less"
+			}
+		}
+	},
+	watch: {
+		files: "./less/**/*.less",
+		tasks: ["less"]
+	}
+});
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Toolkit
+
+Toolkit provides a project workspace for quick automated LESS file compiling, simply run the following commands and edit away:
+
+```
+npm install
+grunt watch
+```
+
+You can also run ```grunt less``` for a single compile
+
